421b0121235c917c54c58e66818b151510887400
IryArkhy <37odri@gmail.com>
2020-02-02 15:00:32 +0200

    all done

--- contacts.js
+++ contacts.js
@@ -1,51 +1,24 @@
 const path = require("path");
 const fs = require("fs");
 const shortid = require("shortid");
-const { promisify } = require("util");
 
-// const contactsPath = path.resolve("db", "contacts.json");
 const contactsPath = path.join(__dirname, "db", "contacts.json");
-// const contacts = JSON.parse(fs.readFile(contactsPath, { encoding: "utf-8" }));
 
-//Var1-----------
-// function listContacts() {
-//   const contacts = fs.readFileSync(contactsPath, { encoding: "utf-8" });
-//   return JSON.parse(contacts);
-// }
-
-//Var2-----------
 const listContacts = () =>
   JSON.parse(fs.readFileSync(contactsPath, { encoding: "utf-8" }));
 
-//Var 1---------
-// const getContactById = contactId =>
-//   listContacts().find(contact => {
-//     if (typeof contact.id === "number") return contact.id === Number(contactId);
-//     return contact.id === contactId;
-//   });
-
-//Var 2-----------
-
 const getContactById = contactId =>
-  listContacts().find(contact =>
-    typeof contact.id === "number"
-      ? contact.id === Number(contactId)
-      : contact.id === contactId
-  );
+  listContacts().find(contact => contact.id === contactId);
 
 function removeContact(contactId) {
-  const newContactsList = listContacts().filter(contact =>
-    typeof contact.id === "number"
-      ? contact.id !== Number(contactId)
-      : contact.id !== contactId
+  const newContactsList = listContacts().filter(
+    contact => contact.id !== contactId
   );
-
   fs.writeFileSync(contactsPath, JSON.stringify(newContactsList), err => {
     if (err) return console.log(err);
   });
 }
 
-///Ended here - the function doesn't work
 const addContact = (name, email, phone) => {
   const contacts = listContacts();
   const newContact = {
@@ -54,42 +27,44 @@ const addContact = (name, email, phone) => {
     email,
     phone
   };
-  const newContactsList = contacts.push(newContact);
-  fs.writeFileSync(contactsPath, JSON.stringify(newContactsList), err => {
+  contacts.push(newContact);
+
+  fs.writeFileSync(contactsPath, JSON.stringify(contacts), err => {
     if (err) return console.log(err);
   });
   return newContact;
 };
 
-// function addContact(name, email, phone) {
-//   fs.readFile(contactsPath, "utf8", (err, res) => {
-//     if (err) {
-//       console.log(err);
-//       return;
-//     }
-//     const contacts = JSON.parse(res);
-//     const latsContact = contacts[contacts.length - 1];
+const updateContact = (contactId, name, email, phone) => {
+  const contacts = [...listContacts()];
 
-//     const contact = {
-//       id: latsContact.id + 1,
-//       name,
-//       email,
-//       phone
-//     };
-//     contacts.push(contact);
-//     fs.writeFile(contactsPath, JSON.stringify(contacts), err => {
-//       if (err) {
-//         console.log(err);
-//         return;
-//       }
-//       console.log("Contact is Added");
-//     });
-//   });
-// }
+  contacts.forEach(contact => {
+    if (contact.id === contactId) {
+      name ? (contact.name = name) : contact.name;
+      email ? (contact.email = email) : contact.email;
+      phone ? (contact.phone = phone) : contact.phone;
+    }
+  });
+  fs.writeFileSync(contactsPath, JSON.stringify(contacts));
+  return contacts;
+};
 
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
-  addContact
+  addContact,
+  updateContact
 };
+
+//Var2-----------
+// function listContacts() {
+//   const contacts = fs.readFileSync(contactsPath, { encoding: "utf-8" });
+//   return JSON.parse(contacts);
+// }
+
+// const getContactById = contactId =>
+//   listContacts().find(contact => {
+//     if (typeof contact.id === "number") return contact.id === Number(contactId);
+//     return contact.id === contactId;
+//   });
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const argv = require("yargs").argv;
 const express = require("express");
 const app = express();
-const fs = require("fs");
 const cors = require("cors");
 const logger = require("morgan");
 const listActions = require("./contacts");
@@ -27,13 +25,13 @@ app.get("/api/contacts/:contactId", (req, res) => {
 });
 app.post("/api/contacts", (req, res) => {
   const { name, email, phone } = req.body;
-  const newContact = listActions.addContact(name, email, phone);
   if (!name || !phone || !email) {
     return res.status(422).json({
       message: "Some fields are missing"
     });
   }
-  res.status(201).json(newContact);
+  const newContact = listActions.addContact(name, email, phone);
+  res.status(201).json({ newContact });
 });
 
 app.delete("/api/contacts/:contactId", (req, res) => {
@@ -41,7 +39,26 @@ app.delete("/api/contacts/:contactId", (req, res) => {
   listActions.removeContact(contactId);
   res.status(200).json({ contactId, message: "Contact has been removed" });
 });
-app.patch("/api/contacts/:contactId");
+
+app.patch("/api/contacts", (req, res) => {
+  res.status(400).json({
+    message: "Method @PATCH not have userId in path"
+  });
+});
+
+app.patch("/api/contacts/:contactId", (req, res) => {
+  const { contactId } = req.params;
+  const { name, email, phone } = req.body;
+
+  if (!name & !phone & !email) {
+    return res.status(400).json({
+      message: "Some fields are missing"
+    });
+  }
+
+  listActions.updateContact(contactId, name, email, phone);
+  res.status(200).json({ contact: listActions.getContactById(contactId) });
+});
 
 app.use("*", (req, res) =>
   res.status(404).send(`This path ${req.url} cannot be found`)
@@ -52,28 +69,3 @@ app.use((err, req, res) =>
   })
 );
 app.listen(port, () => console.log(`Server started on port ${port}`));
-
-// function invokeAction({ action, id, name, email, phone }) {
-//   switch (action) {
-//     case "list":
-//       listActions.listContacts();
-//       break;
-
-//     case "get":
-//       listActions.getContactById(id);
-//       break;
-
-//     case "add":
-//       listActions.addContact(name, email, phone);
-//       break;
-
-//     case "remove":
-//       listActions.removeContact(id);
-//       break;
-
-//     default:
-//       console.warn("\x1B[31m Unknown action type!");
-//   }
-// }
-
-// invokeAction(argv);
